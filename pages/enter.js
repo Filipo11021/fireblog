0f5f6e2c9d4f1607a6e6ac6ebe4050315caa0680
Filipo <filip@filip.home>
2022-08-29 04:06:53 +0200

    add login page redirection

--- pages/enter.js
+++ pages/enter.js
@@ -1,12 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { UserCtx } from "../lib/ctx";
 import SignOutBtn from "../components/auth/SignOutBtn";
 import SignInBtn from "../components/auth/SignInBtn";
 import UsernameForm from "../components/usernameForm/UsernameForm";
+import { useRouter } from "next/router";
 
 const EnterPage = () => {
   const { user, username } = useContext(UserCtx);
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    if (username && user) router.push("/");
+  }, [username, user, router]);
+
   return (
     <main>
       {user ? !username ? <UsernameForm /> : <SignOutBtn /> : <SignInBtn />}
